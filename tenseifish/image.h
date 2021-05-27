5f9f374e51f7ec8fd1700594010247892c625d6e
shiromaharuto <haruto-shiroma-19f@stu.kbc.ac.jp>
2021-05-27 13:27:11 +0900

    コメント追加

--- tenseifish/image.h
+++ tenseifish/image.h
@@ -1,36 +1,36 @@
 #pragma once
-int Gametitle;
-int Gamehelp;
-int Gamegiyo;
-int Gamegiyo2;
-int Gamegiyo3;
-int Gamerule;
-int Gamerule2;
+int Gametitle;	//ゲームタイトル画像
+int Gamehelp;	//ゲームヘルプ画像
+int Gamegiyo;	//ゲーム概要画像
+int Gamegiyo2;	//ゲーム概要画像2
+int Gamegiyo3;	//ゲーム概要画像3
+int Gamerule;	//ゲームルール画像
+int Gamerule2;	//ゲームルール画像2
 int GameMS;		// 音量調整
-int pauseImage;
+int pauseImage;	//ポーズ画像
 int Corsol;	// カーソル画像変数
-int Corsol2;
-int StageImage;
+int Corsol2;// カーソル画像2
+int StageImage;	//ステージ画像
 int Iwa[3];//手前の背景
 int IwaHaba = 7732;//手前の背景の幅	
 int Iwaspeed = 0;//手前のスピード
 int Umispeed = 0;//海のスピード
 int MenuNo = 0;	// カーソルの番号
 int MenuNo1 = 0;//カーソルの番号
-int Life;
+int Life;	// 体力画像
 int Gameclear[11];		//エンディングの分岐の画像
 int ClearZukan[11];		//図鑑の画像
 int MZukan[11];			//未登録の図鑑の画像
-int Gameover;
-int Gameover2;
-int Gameover3;
-int Meter[3][6];
+int Gameover;	//ゲームオーバー画像
+int Gameover2;	//ゲームオーバー画像2
+int Gameover3;	//ゲームオーバー画像3
+int Meter[3][6];	//餌ゲージ画像
 int sakana[3][10] = { 0 };		//魚の画像を格納する配列
 int anime[] = { 0,1,0,2 };		//泳ぐときのアニメーションの番号 尾びれが真ん中、上、真ん中、下
 int	feedImage[3][3];	//左側を向く餌の画像を格納する変数 1エビ,2アジ,3イカ
 int feedImageur[4][3];	//右側を向く餌の画像を格納する変数 1エビ,2アジ,3イカ4ミノカサゴ
 int EnemyImage[2][3];	//敵の画像を格納する変数 1クラゲ,2ミノカサゴ
-int fishdie[3][7] = { 0 };
+int fishdie[3][7] = { 0 };	//死亡アニメーション画像
 int BOSSROAD;           //ボスへの道
 int BOSSMETER;           //マグロへの道の表し
 int BOSSY;              //マグロへの道の表し（矢印）
